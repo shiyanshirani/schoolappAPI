b27342ab5f231e58a173f4a92b1074f5b788e0e3
shiyanshirani <shiyan99s@gmail.com>
2021-07-04 12:05:16 +0530

    UserProfile 1-1-Field with User

--- schoolapp/users/models.py
+++ schoolapp/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=150)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
+    email = models.EmailField(max_length=150, blank=True)
+    address = models.TextField(max_length=250, blank=True)
+    is_teacher = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.user}"
