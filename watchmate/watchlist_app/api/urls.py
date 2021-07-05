563f58f906a33be35d7ebdc7558e083c0ce8933b
shiyanshirani <shiyan99s@gmail.com>
2021-07-05 21:59:23 +0530

    WatchListAV and WatchListDetailAV same for StreamPlatformAV and ...

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from watchlist_app.api.views import MovieListAV, MovieDetailAV 
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV
 
 urlpatterns = [
-    path('list/', MovieListAV.as_view(), name='movie-list'),
-    path('<int:pk>/', MovieDetailAV.as_view(), name='individual-movie'),
+    path('list/', WatchListAV.as_view(), name='movie-list'),
+    path('<int:pk>/', WatchListDetailAV.as_view(), name='individual-movie'),
+
+    path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream'),
 ]
