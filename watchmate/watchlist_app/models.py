88dcd99ff6d2ce5b7b249592f5b4e50a029fb73e
shiyanshirani <shiyan99s@gmail.com>
2021-07-05 22:02:14 +0530

    WatchList models.ForeignKey(StreamPlatform)

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Movie(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=30)
+    about = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
 
     def __str__(self):
         return self.name
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
