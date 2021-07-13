9bba913a90134fed83ad42c00f1f3ebe9464b02b
shiyanshirani <shiyan99s@gmail.com>
2021-07-13 22:39:19 +0530

    existing routes commented for testing purposes

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='individual-stream'),
 
     path('stream/review/', ReviewList.as_view(), name='review-list'),
+    
 
     path('stream/review/<int:pk>/', ReviewList.as_view(), name='review-list'),
     path('stream/<int:pk>/review/', ReviewDetail.as_view(), name='review-detail'),
