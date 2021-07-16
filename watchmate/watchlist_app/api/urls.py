56d928c543981189dfa603d63cbdd6752c73c5b3
shiyanshirani <shiyan99s@gmail.com>
2021-07-16 18:29:10 +0530

    watch/stream/review/<int:pk>/review/ for POSTing new review

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatformAV, StreamPlatformDetailAV, ReviewList, ReviewDetail, ReviewCreate
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -8,9 +8,10 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='individual-stream'),
 
-    path('stream/review/', ReviewList.as_view(), name='review-list'),
+    # path('stream/review/', ReviewList.as_view(), name='review-list'),
     
 
     path('stream/review/<int:pk>/', ReviewList.as_view(), name='review-list'),
     path('stream/<int:pk>/review/', ReviewDetail.as_view(), name='review-detail'),
+    path('stream/<int:pk>/review-create/', ReviewCreate.as_view(), name='review-create')
 ] 
