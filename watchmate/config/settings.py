aba68fd14215ee55cdfafaf5d6e1a13c75a48860
shiyanshirani <shiyan99s@gmail.com>
2021-08-17 23:18:36 +0530

    removed login

--- watchmate/config/settings.py
+++ watchmate/config/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_URL = 'logout'
 LOGOUT_REDIRECT_URL = 'login'
