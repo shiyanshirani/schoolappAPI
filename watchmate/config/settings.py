8d891d72b8fbbfd37a197050fd9eeb5be8f9a8e5
shiyanshirani <shiyan99s@gmail.com>
2021-08-17 23:20:21 +0530

    removed facebook_social_auth_key

--- watchmate/config/settings.py
+++ watchmate/config/settings.py
@@ -125,5 +125,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-SOCIAL_AUTH_FACEBOOK_SECRET = config('SOCIAL_AUTH_FACEBOOK_SECRET')
