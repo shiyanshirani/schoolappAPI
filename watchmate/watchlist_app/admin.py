6b615e955cde963863229c5e26a3250370501767
shiyanshirani <shiyan99s@gmail.com>
2021-08-18 21:28:28 +0530

    review registered

--- watchmate/watchlist_app/admin.py
+++ watchmate/watchlist_app/admin.py
@@ -4,4 +4,4 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 # Register your models here.
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
-)
+admin.site.register(Review)
