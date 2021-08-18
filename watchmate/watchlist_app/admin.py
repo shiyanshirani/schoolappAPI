8c753e5802926fae9334f8b259385c1d48e9f7b4
shiyanshirani <shiyan99s@gmail.com>
2021-08-18 21:27:27 +0530

    unregisted

--- watchmate/watchlist_app/admin.py
+++ watchmate/watchlist_app/admin.py
@@ -2,6 +2,3 @@ from django.contrib import admin
 from watchlist_app.models import WatchList, StreamPlatform, Review
 
 # Register your models here.
-admin.site.register(WatchList)
-admin.site.register(StreamPlatform)
-admin.site.register(Review)
