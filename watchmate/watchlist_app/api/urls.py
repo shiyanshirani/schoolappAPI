8cb96e855409c8a6eb0a112b9b5cc256cb8ddf64
shiyanshirani <shiyan99s@gmail.com>
2021-08-20 23:18:13 +0530

    BUG: crashes

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', WatchListDetailAV.as_view(), name='individual-movie'),
 
-#     path('stream/', StreamPlatformAV.as_view(), name='stream'),
+    path('stream/', StreamPlatformAV.as_view(), name='stream'),
 #     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(),
      #     name='individual-stream'),
 
