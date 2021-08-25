d0190e3fdea1d71cc0ab32857c148e9cabbf58bf
shiyanshirani <shiyan99s@gmail.com>
2021-08-25 23:15:52 +0530

    bug 2

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(),
-     #     name='individual-stream'),
+         name='individual-stream'),
 
-#     path('stream/review/', ReviewList.as_view(), name='review-list'),
+    path('stream/review/', ReviewList.as_view(), name='review-list'),
 
 
     path('stream/review/<int:pk>/', ReviewList.as_view(), name='review-list'),
